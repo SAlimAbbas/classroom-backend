57c716a7898da219d3c5573a677f678e390acd45
SAlimAbbas <alimabbas44891@gmail.com>
2026-02-04 17:44:32 +0530

    implement get all subject route

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,27 @@
 import express from "express";
+import cors from "cors";
+
+import subjectsRouter from "./routes/subject";
 
 const app = express();
 const port = process.env.PORT || 8000;
 
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL || "http://localhost:5173",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  }),
+);
+
 app.use(express.json());
 
+app.use("/api/subjects", subjectsRouter);
+
 app.get("/", (req, res) => {
-    res.send("Hello Welcome to the backend!");
-})
+  res.send("Hello Welcome to the backend!");
+});
 
 app.listen(port, () => {
-    console.log(`Server started at http://localhost:${port}`);
-})
+  console.log(`Server started at http://localhost:${port}`);
+});
